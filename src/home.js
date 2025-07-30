56f621b8c1830a87bdb2c5e0beef46dc7d141089
leetbeet <136890775+leetbeet@users.noreply.github.com>
2025-07-30 12:10:33 +0800

    Add module to load homepage

---
+++ src/home.js
@@ -0,0 +1,30 @@
+import tuscanImg from "./tuscan-dish.jpg";
+
+export const loadHome = () => {
+    const aboutHeader = document.createElement("h2");
+    aboutHeader.textContent = "About";
+    const aboutDesc = document.createElement("p");
+    aboutDesc.textContent = "Tramonto ('Sunset') is a charming, family-owned Italian restaurant in Florence, known for its warm ambiance and stunning evening views. Inspired by the golden Tuscan sunsets, Tramonto blends traditional recipes with fresh, seasonal ingredients.";
+
+    const img = document.createElement("img");
+    img.src = tuscanImg;
+    img.alt = "Tuscan dish"
+
+    const hoursHeader = document.createElement("h2");
+    hoursHeader.textContent = "Hours";
+
+    const hoursList = document.createElement("ul");
+    const hoursListOne = document.createElement("li");
+    hoursListOne.textContent = "Mon–Sat: 12:00–10:30";
+    const hoursListTwo = document.createElement("li");
+    hoursListTwo.textContent = "Sun: Closed";
+    hoursList.append(hoursListOne, hoursListTwo);
+
+    const locationHeader = document.createElement("h2");
+    locationHeader.textContent = "Location";
+    const locationDesc = document.createElement("p");
+    locationDesc.textContent = "Via delle Rose 18, Florence, Italy";
+
+    const content = document.querySelector("#content");
+    content.append(aboutHeader, aboutDesc, img, hoursHeader, hoursList, locationHeader, locationDesc);
+}
--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
-import "./style.css"
+import "./style.css";
+import "./tuscan-dish.jpg"
+import { loadHome } from "./home";
+
+loadHome();
+
